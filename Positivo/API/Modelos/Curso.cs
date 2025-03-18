63967e584f3a32a265b6d759071b5745865737c9
PHABLO FRANCA <phablo.franca@cs.up.edu.br>
2025-03-17 21:39:01 -0300

    Adicionando cursos

---
+++ Positivo/API/Modelos/Curso.cs
@@ -0,0 +1,11 @@
+namespace Namespace;
+public class Curso
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Curso(int id, string name) {
+        Id = id;
+        Name = name;
+    }
+}
